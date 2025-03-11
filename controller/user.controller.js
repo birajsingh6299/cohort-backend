e66665b06d25180edc2abab75bf335b69997feca
Biraj Singh <birajsingh176@gmail.com>
2025-03-11 12:02:08 +0000

    change get request to post in user.routes.js, define register functionality in user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,49 @@
+import User from "../model/user.model.js"
+import crypto from "crypto"
+
 const registerUser = async (req, res) => {
-    res.send("Registered");
+    const {name, email, password} = req.body
+    
+    if (!name || !email || !password) {
+        res.status(400).json({
+            message:"All fields are required"
+        })
+    }
+
+    try {
+        const existingUser = await User.findOne({email})
+
+        if (existingUser) {
+            return res.status(400).json({
+                message:"User already exists"  
+            })
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+
+        if(!user) {
+            return res.status(400).json({
+                message:"User not registered",  
+        });
+
+            const token = crypto.randomBytes(32).toString("hex");
+        }
+
+    } catch (error) {
+
+    }
+    //get data
+    //validate
+    //check if user already exists
+    // create a user in database
+    // create a verification token
+    // save token in database
+    // send token as email to user
+    // send success status to user
 };
 
 export { registerUser };
